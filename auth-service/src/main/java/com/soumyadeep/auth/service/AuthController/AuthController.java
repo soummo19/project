027dd69ba7998dde4856aa3729dc606b9e112c39
Soumyadeep <soumyadeeplaskar.2000@gmail.com>
2024-11-10 00:14:17 +0530

    added sendOTP , getOTP endpoint

--- auth-service/src/main/java/com/soumyadeep/auth/service/AuthController/AuthController.java
+++ auth-service/src/main/java/com/soumyadeep/auth/service/AuthController/AuthController.java
@@ -1,7 +1,6 @@
 package com.soumyadeep.auth.service.AuthController;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,14 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 import com.soumyadeep.auth.service.AuthModels.OtpRequestBody;
 
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("api/v1/auth")
 public class AuthController {
 
+    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+
     @PostMapping("/sendOTP")
     public ResponseEntity<String> sendOTP(@Valid @RequestBody OtpRequestBody requestBody){
-        
+        log.info("Received Request to send OTP");
         return ResponseEntity.ok("OTP SENT");
     }
 
